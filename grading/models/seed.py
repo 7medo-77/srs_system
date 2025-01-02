394d05b64b6d03b9430d8520b681158201b558e0
hmedo <hmedo@7medo>
2025-01-02 17:46:30 +0200

    improved seed script to prevent recurrent seeds

--- grading/models/seed.py
+++ grading/models/seed.py
@@ -58,12 +58,15 @@ fake_department = {
  }
 
 # Generate Departments
+departmentResult = Department.objects.all()
 departments = []
-for key in fake_department.keys():
-    department = Department(
-        name=key, head_of_department=fake.name()
-    )
-    departments.append(department)
+
+if len(departmentResult) == 0:
+    for key in fake_department.keys():
+        department = Department(
+            name=key, head_of_department=fake.name()
+        )
+        departments.append(department)
 
 # print(departments)
 # for dept in departments:
@@ -74,19 +77,21 @@ Department.objects.bulk_create(departments)  # Bulk create departments
 
 departments = Department.objects.all()
 
-# Generate Instructors
+instructorResult = Instructor.objects.all()
 instructors = []
-for department in departments:
-    instructor_count = random_within_range(8, 15)
-    for _ in range(instructor_count):
-        instructor = Instructor(
-            first_name=fake.first_name(),
-            last_name=fake.last_name(),
-            email=fake.email(),
-            phone_number=fake.lexify(text=('01?-????-????'), letters="0123456789"),
-            department=department,
-        )
-        instructors.append(instructor)
+# Generate Instructors
+if len(instructorResult) == 0:
+    for department in departments:
+        instructor_count = random_within_range(8, 15)
+        for _ in range(instructor_count):
+            instructor = Instructor(
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                email=fake.email(),
+                phone_number=fake.lexify(text=('01?-????-????'), letters="0123456789"),
+                department=department,
+            )
+            instructors.append(instructor)
 
 # # print( instructors )
 # for instructor in instructors:
@@ -96,115 +101,120 @@ for department in departments:
 Instructor.objects.bulk_create(instructors)  # Bulk create instructors
 
 # Generate Courses (considering department and instructor relationships)
+courseResult = Course.objects.all()
 courseArray = []
 
-for dep, courses in fake_department.items():
-# for dep in departments:
-    for index, course in enumerate(courses):
-        course_code_initials = ''.join([name[0] for name in course.split(' ') if name[0].isupper()])
-        result_department = Department.objects.get(name=dep)
+if len(courseResult) == 0:
+    for dep, courses in fake_department.items():
+        for index, course in enumerate(courses):
+            course_code_initials = ''.join([name[0] for name in course.split(' ') if name[0].isupper()])
+            result_department = Department.objects.get(name=dep)
 
-        courseObject = Course(
-            course_name=fake_department[dep][index],
-            # course_code=fake.unique.lexify(text="CS###"),
-            course_code=fake.unique.lexify(text=f"{course_code_initials}???", letters="0123456789"),
-            # description=fake.text(),
-            credits=random_within_range(1, 5),
-            department=result_department,
-        )
-        courseArray.append(courseObject)
+            courseObject = Course(
+                course_name=fake_department[dep][index],
+                # course_code=fake.unique.lexify(text="CS###"),
+                course_code=fake.unique.lexify(text=f"{course_code_initials}???", letters="0123456789"),
+                # description=fake.text(),
+                credits=random_within_range(1, 5),
+                department=result_department,
+            )
+            courseArray.append(courseObject)
 
-Course.objects.bulk_create(courseArray)  # Bulk create courses with assigned instructors
+    Course.objects.bulk_create(courseArray)  # Bulk create courses with assigned instructors
 
-instructors = Instructor.objects.all()
+    instructors = Instructor.objects.all()
 
-for index, inst in enumerate(instructors):
-    courses = list(inst.department.courses.all())
-    inst.courses.set(random.sample(courses, k=random.randint(1, 3)))
-    print([course.course_name for course in inst.courses.all() ])
+    for index, inst in enumerate(instructors):
+        courses = list(inst.department.courses.all())
+        inst.courses.set(random.sample(courses, k=random.randint(1, 3)))
+        print([course.course_name for course in inst.courses.all() ])
 
 
 # print(courseArray)
 
 # Generate Students
+studentResult = Student.objects.all()
 students = []
 enrollments = []
-for department in Department.objects.all():
-    courses = list(department.courses.all())
+if len(studentResult) == 0:
+    for department in Department.objects.all():
+        courses = list(department.courses.all())
 
-    for i in range(random.randint(150, 250)):
-        student = Student.objects.create(
-            first_name=fake.first_name(),
-            last_name=fake.last_name(),
-            email=fake.email(),
-            # phone_number=fake.phone_number(),
-            phone_number=fake.lexify(text=('01?-????-????'), letters="0123456789"),
-            # enrollment_date=fake.date_between(start_date=datetime.date(2019, 1, 1) , end_date=datetime.date(2024, 9, 1)),
-            major=fake.job(),
-            date_of_birth=fake.date_between(start_date=datetime.date(1997, 1, 1) , end_date=datetime.date(2007, 12, 30)),
-        )
-        for course in courses:
-            float_num = random.uniform(0,1)
-            probability = round(float_num)
-            # print(course.instructor)
-
-            if probability == 0:
-                continue
-
-            instructors = list(course.instructors.all())
-            fake_year = random_within_range(2020, 2024)
-            fake_semester=fake.random.choice(["Fall", "Spring", "Summer"]),
-
-            if fake_semester == "fall":
-                fake_date = fake.date_between(start_date=datetime.date(fake_year, 8, 1) , end_date=datetime.date(fake_year, 10, 15))
-            elif fake_semester == "spring":
-                fake_date = fake.date_between(start_date=datetime.date(fake_year, 1, 1) , end_date=datetime.date(fake_year, 2, 20))
-            else:
-                fake_date = fake.date_between(start_date=datetime.date(fake_year, 6, 1) , end_date=datetime.date(fake_year, 7, 15))
-
-            fake_grade=round(random.uniform(60, 99), 2)
-            # print(fake_grade)
-
-            enrollment = StudentCourseEnrollment.objects.create(
-                student=student,
-                course=course,
-                instructor=random.choice(instructors),
-                date=fake_date,
-                semester=fake_semester,
-                academic_year=fake_year,
-                grade=fake_grade
+        for i in range(random.randint(150, 250)):
+            student = Student.objects.create(
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                email=fake.email(),
+                # phone_number=fake.phone_number(),
+                phone_number=fake.lexify(text=('01?-????-????'), letters="0123456789"),
+                # enrollment_date=fake.date_between(start_date=datetime.date(2019, 1, 1) , end_date=datetime.date(2024, 9, 1)),
+                major=fake.job(),
+                date_of_birth=fake.date_between(start_date=datetime.date(1997, 1, 1) , end_date=datetime.date(2007, 12, 30)),
             )
-            enrollments.append(enrollments)
-        students.append(student)
+            for course in courses:
+                float_num = random.uniform(0,1)
+                probability = round(float_num)
+                # print(course.instructor)
+
+                if probability == 0:
+                    continue
+
+                instructors = list(course.instructors.all())
+                fake_year = random_within_range(2020, 2024)
+                fake_semester=fake.random.choice(["Fall", "Spring", "Summer"]),
+
+                if fake_semester == "fall":
+                    fake_date = fake.date_between(start_date=datetime.date(fake_year, 8, 1) , end_date=datetime.date(fake_year, 10, 15))
+                elif fake_semester == "spring":
+                    fake_date = fake.date_between(start_date=datetime.date(fake_year, 1, 1) , end_date=datetime.date(fake_year, 2, 20))
+                else:
+                    fake_date = fake.date_between(start_date=datetime.date(fake_year, 6, 1) , end_date=datetime.date(fake_year, 7, 15))
+
+                fake_grade=round(random.uniform(60, 99), 2)
+                # print(fake_grade)
+
+                enrollment = StudentCourseEnrollment.objects.create(
+                    student=student,
+                    course=course,
+                    instructor=random.choice(instructors),
+                    date=fake_date,
+                    semester=fake_semester,
+                    academic_year=fake_year,
+                    grade=fake_grade
+                )
+                enrollments.append(enrollments)
+            students.append(student)
+    Student.objects.bulk_create(students)  # Bulk create students
 
 # print(students)
 for stu in students:
     print(stu.__repr__())
-# Student.objects.bulk_create(students)  # Bulk create students
 
-# # Generate Student Course Enrollments (considering student enrollment range)
-# enrollments = []
-# for student in students:
-#     course_choices = courses.copy()  # Copy the list to avoid duplicates
-#     enrollment_count = random_within_range(5, 7)
-#     for _ in range(enrollment_count):
-#         chosen_course = course_choices.pop(randint(0, len(course_choices) - 1))
-#         enrollment = StudentCourseEnrollment(
-#             student=student,
-#             course=chosen_course,
-#             instructor=chosen_course.instructors.first(),  # Pick one random instructor
-#             date=fake.date(),
-#             semester=fake.random.choice(["Fall", "Spring", "Summer"]),
-#             academic_year=random_within_range(2022, 2024),
-#             grade=round(fake.random.uniform(60, 100), 2),  # Random grades between 60 and 100 (rounded to 2 decimal places)
-#         )
-#         enrollments.append(enrollment)
-# print(enrollments)
-# # StudentCourseEnrollment.objects.bulk_create(enrollments)  # Bulk create student enrollments
+# Generate Student Course Enrollments (considering student enrollment range)
+enrollmentResult = StudentCourseEnrollment.objects.all()
+enrollments = []
+if len( enrollmentResult ) == 0:
+    for student in students:
+        course_choices = courses.copy()  # Copy the list to avoid duplicates
+        enrollment_count = random_within_range(5, 7)
+        for _ in range(enrollment_count):
+            chosen_course = course_choices.pop(randint(0, len(course_choices) - 1))
+            enrollment = StudentCourseEnrollment(
+                student=student,
+                course=chosen_course,
+                instructor=chosen_course.instructors.first(),  # Pick one random instructor
+                date=fake.date(),
+                semester=fake.random.choice(["Fall", "Spring", "Summer"]),
+                academic_year=random_within_range(2022, 2024),
+                grade=round(fake.random.uniform(60, 100), 2),  # Random grades between 60 and 100 (rounded to 2 decimal places)
+            )
+            enrollments.append(enrollment)
+    print(enrollments)
+    StudentCourseEnrollment.objects.bulk_create(enrollments)  # Bulk create student enrollments
 
 print(f"Generated {len(departments)} Departments")
 print(f"Generated {len(instructors)} Instructors")
-print(f"Generated {len(courses)} Courses")
+print(f"Generated {len(courseArray)} Courses")
 print(f"Generated {len(students)} Students")
 print(f"Generated {len(enrollments)} Enrollments")
 # # # print(
